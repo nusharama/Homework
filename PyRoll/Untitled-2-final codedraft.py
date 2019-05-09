ce197a284b576a6a1070f55992af243f995fd4b6
Nush Sash <anusha.rama@gmail.com>
2019-05-09 15:40:22 -0400

    work saved so far

---
+++ PyRoll/Untitled-2-final codedraft.py
@@ -0,0 +1,112 @@
+import csv
+import os
+
+csvpath = os.path.join(".", "desktop", "python-challenge", "PyRoll", 'election_data.csv')
+# The total number of votes included in the dataset
+total_votes = 0
+khan_votes = 0
+correy_votes = 0
+li_votes = 0
+otooley_votes = 0
+# each_vote = []
+# candidate_list = []
+percentage = []
+khan_percent = 0
+correy_percent = 0
+li_percent = 0
+otooley_percent = 0
+
+# Open & Read CSV File
+with open(csvpath, newline='') as csvfile:
+
+    # CSV Reader Specifies Delimiter & Variable That Holds Contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    print(csvreader)
+
+    # Read The Header Row First (Skip This Step If There Is No Header)
+    csv_header = next(csvreader)
+    for row in csv.reader(csvfile):
+         # Calculate Total Number Of Votes Cast
+        total_votes += 1
+        
+        # Calculate Total Number Of Votes Each Candidate Won
+        if (row[2] == "Khan"):
+            khan_votes += 1
+        elif (row[2] == "Correy"):
+            correy_votes += 1
+        elif (row[2] == "Li"):
+            li_votes += 1
+        else:
+            otooley_votes += 1
+            
+    # Calculate Percentage Of Votes Each Candidate Won
+    khan_percent = khan_votes / total_votes
+    correy_percent = correy_votes / total_votes
+    li_percent = li_votes / total_votes
+    otooley_percent = otooley_votes / total_votes
+
+    # for x in total_votes:
+    #     percentage.append((int(x)/total_votes)*100)
+    
+    # Calculate Winner Of The Election Based On Popular Vote
+    winner = max(khan_votes, correy_votes, li_votes, otooley_votes)
+
+    if winner == khan_votes:
+        winner_name = "Khan"
+    elif winner == correy_votes:
+        winner_name = "Correy"
+    elif winner == li_votes:
+        winner_name = "Li"
+    else:
+        winner_name = "O'Tooley" 
+
+# Print Analysis
+print(f"Election Results")
+print(f"---------------------------")
+print(f"Total Votes: {total_votes}")
+print(f"---------------------------")
+print(f"Khan: {khan_percent:.3%} ({khan_votes})")
+print(f"Correy: {correy_percent:.3%} ({correy_votes})")
+print(f"Li: {li_percent:.3%} ({li_votes})")
+print(f"OTooley: {otooley_percent:.3%} ({otooley_votes})")
+print(f"---------------------------")
+print(f"Winner: {winner_name}")
+print(f"---------------------------")
+# for x in range(len(candidate_list)):
+#         print.append(candidate_list[x] + ': ' + str(round(percentage[x],1)) + '% ' + '(' + str(each_vote[x]) + ')')
+        
+# # Dependencies
+import os
+import csv
+
+# Specify the file to write to
+output_path = os.path.join(".", "desktop", "python-challenge","PyRoll", 'Election_Results.csv')
+
+# Open the file using "write" mode. Specify the variable to hold the contents
+with open(output_path, 'w', newline='') as csvfile:
+
+    # Initialize csv.writer
+    csvwriter = csv.writer(csvfile, delimiter=',', )
+
+    # Write the first row (column headers)
+    csvwriter.writerow(['Total Votes', 
+    'Khan', 
+    'Correy', 
+    'Li', 
+    'O''Tooley', 
+    'Winner'])
+
+# Write the second row
+    # csvwriter.writerow(f"Election Results")
+    # csvwriter.writerow(f"---------------------------")
+    # csvwriter.writerow(f"Total Votes: {total_votes}")
+    # csvwriter.writerow(f"---------------------------")
+    csvwriter.writerow(f"{khan_percent:.3%} {khan_votes})"),(f"{correy_percent:.3%} ({correy_votes})"), 
+    (f"{li_percent:.3%} ({li_votes})"), (f"{otooley_percent:.3%} ({otooley_votes})"), 
+    (f"{winner_name}")
+    # csvwriter.writerow(f"---------------------------")
+
+    
+    csvwriter.writerow([len(total_votes), len("khan_percent:.3% \n"), khan_votes, len("correy_percent:.3% \n"), correy_votes, 
+    len("li_percent:.3% \n"), li_votes, len("otooley_percent:.3% \n"), otooley_votes, winner_name])
