cefe5d895ff768e718f386dfbf94450a80ae5269
Nush Sash <anusha.rama@gmail.com>
2019-05-10 21:02:15 -0400

    changes saved

--- PyRoll/main.py
+++ PyRoll/main.py
@@ -2,6 +2,7 @@ import csv
 import os
 
 csvpath = os.path.join(".", "desktop", "python-challenge", "PyRoll", 'election_data.csv')
+
 # The total number of votes included in the dataset
 total_votes = 0
 khan_votes = 0
@@ -18,18 +19,18 @@ otooley_percent = 0
 # Open & Read CSV File
 with open(csvpath, newline='') as csvfile:
 
-    # CSV Reader Specifies Delimiter & Variable That Holds Contents
+# CSV Reader Specifies Delimiter & Variable That Holds Contents
     csvreader = csv.reader(csvfile, delimiter=',')
 
     print(csvreader)
 
-    # Read The Header Row First (Skip This Step If There Is No Header)
+# Read The Header Row First (Skip This Step If There Is No Header)
     csv_header = next(csvreader)
     for row in csv.reader(csvfile):
          # Calculate Total Number Of Votes Cast
         total_votes += 1
         
-        # Calculate Total Number Of Votes Each Candidate Won
+# Calculate Total Number Of Votes Each Candidate Won
         if (row[2] == "Khan"):
             khan_votes += 1
         elif (row[2] == "Correy"):
@@ -39,14 +40,14 @@ with open(csvpath, newline='') as csvfile:
         else:
             otooley_votes += 1
             
-    # Calculate Percentage Of Votes Each Candidate Won
+# Calculate Percentage Of Votes Each Candidate Won
     khan_percent = khan_votes / total_votes
     correy_percent = correy_votes / total_votes
     li_percent = li_votes / total_votes
     otooley_percent = otooley_votes / total_votes
 
         
-    # Calculate Winner Of The Election Based On Popular Vote
+# Calculate Winner Of The Election Based On Popular Vote
     winner = max(khan_votes, correy_votes, li_votes, otooley_votes)
 
     if winner == khan_votes:
@@ -71,7 +72,7 @@ print(f"---------------------------")
 print(f"Winner:{winner_name}")
 print(f"---------------------------")
 
-# # Dependencies
+# Dependencies
 import os
 import csv
 
@@ -81,9 +82,9 @@ output_path = os.path.join(".", "desktop", "python-challenge","PyRoll", 'Electio
 # Open the file using "write" mode. Specify the variable to hold the contents
 with open(output_path, 'w', newline='') as csvfile:
 
-    # Initialize csv.writer
+# Initialize csv.writer
     csvwriter = csv.writer(csvfile, delimiter=',', )
-    # Write the second row
+# Write the second row
     csvwriter.writerow(["Election Results"])
     csvwriter.writerow(["---------------------------"])
     csvwriter.writerow([f"Total Votes: {total_votes}"])
--- pybank/main.py
+++ pybank/main.py
@@ -18,9 +18,11 @@ with open(csvpath, newline='') as csvfile:
    
 # Read The Header Row First (Skip This Step If There Is No Header)
     csv_header = next(csvreader)
+    
     for row in csv.reader(csvfile):
         total_months.append(row[0])
         net_amount.append(int(row[1]))
+
     # print(f"CSV Header: {csv_header}")
 
     for a in range(len(net_amount)-1):
@@ -28,11 +30,12 @@ with open(csvpath, newline='') as csvfile:
      
     Increase_max_change = max(monthly_change)
     Decrease_max_change = min(monthly_change)
-     
+
+         
 print(f'Financial Analysis')
 print('----------------------------------------------------------------------------')
 print(f"Total Months: {len(total_months)}")
-print(f"Net Profit: ${sum(net_amount)}")
+print(f"Net Profit: ${(sum(net_amount))}")
 print(f"Average Change: ${round(sum(monthly_change)/len(monthly_change),2)}")
 print(f"Greatest Increase in Profits: ${str(max(monthly_change))}")
 print(f"Greatest Decrease in Profits: ${str(min(monthly_change))}")
@@ -47,14 +50,14 @@ with open(output_path, 'w', newline='') as csvfile:
     csvwriter = csv.writer(csvfile, delimiter=',')
 
     # Write the first row (column headers)
-csvwriter.writerow(['Total Months', 
+    csvwriter.writerow(['Total Months', 
     'Net Profit', 
     'Average Change', 
     'Greatest Increase in Profits', 
     'Greatest Decrease in Profits'])
 
     # Write the second row
-csvwriter.writerow([len(total_months), 
+    csvwriter.writerow([len(total_months), 
     sum(net_amount), 
     round(sum(monthly_change)/len(monthly_change),2), 
     (max(monthly_change)), 
