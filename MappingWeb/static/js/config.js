f6c1f1a15bfcca4c5681cb703f5d0f151f15365e
Nush Sash <anusha.rama@gmail.com>
2019-08-21 18:26:11 -0400

    mappingweb

---
+++ MappingWeb/static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoiYW51c2hhcmFtYSIsImEiOiJjanpnZmcxN3gwamt0M2Nucmk5b2tzdWN6In0.FRbeXGKG5_bdUuumxTMCuQ";
---
+++ MappingWeb/static/js/logic.js
@@ -0,0 +1,192 @@
+// Selectable backgrounds of our map - tile layers:
+// grayscale background.
+
+var graymap_background = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"http://mapbox.com\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.gray",
+    accessToken: API_KEY
+  });
+
+// Define satelitemap and darkmap layers
+  var satellitemap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.satellite",
+    accessToken: API_KEY
+  });
+
+  var darkmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.dark",
+    accessToken: API_KEY
+  });
+
+// map object to an array of layers we created. // 31.57853542647338,-99.580078125
+// Create the map object with options
+   var map = L.map("map", {
+    center: [31.57853542647338,-99.580078125],
+    zoom: 5,
+    layers: [satellitemap, darkmap, graymap_background]
+    });
+
+    // adding one 'graymap' tile layer to the map.
+    graymap_background.addTo(map);
+
+    // layers for two different sets of data, earthquakes and tectonicplates.
+    var earthquakes = new L.LayerGroup();
+    var tectonicplates = new L.LayerGroup();
+
+    // base layers
+    var baseMaps = {
+        "Satellite": satellitemap,
+        "Grayscale": graymap_background,
+        "DarkMap": darkmap
+    };
+
+    // Create an overlays object to add to the layer control
+    var overlays = {
+        "Earthquakes": earthquakes,
+        "TectonicPlates": tectonicplates
+    };
+
+    // Create a control for our layers, add our overlay layers to it
+    L.control.layers(baseMaps, overlays,{
+        collapsed: false
+    }).addTo(map);
+
+//     // Create a legend to display information about our map
+//     var legend = L.control({
+//     position: "bottomright"
+//   });
+
+
+//   // When the layer control is added, insert a div with the class of "legend"
+// legend.onAdd = function() {
+//     var div = L
+//     .DomUtil
+//     .create("div", "legend");
+//     return div;
+//   };
+
+
+  // Add the info legend to the map
+//   legend.addTo(map);
+
+   // Perform an API call to the Earthquake Information endpoint
+  d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson", function(data) {
+
+
+    function styleInfo(feature) {
+        return {
+          opacity: 1,
+          fillOpacity: 1,
+          fillColor: getColor(feature.properties.mag),
+          color: "#000000",
+          radius: getRadius(feature.properties.mag),
+          stroke: true,
+          weight: 0.5
+        };
+      }
+
+      // Define the color of the marker based on the magnitude of the earthquake.
+  function getColor(magnitude) {
+    switch (true) {
+      case magnitude > 5:
+        return "#ea2c2c";
+      case magnitude > 4:
+        return "#ea822c";
+      case magnitude > 3:
+        return "#ee9c00";
+      case magnitude > 2:
+        return "#eecc00";
+      case magnitude > 1:
+        return "#d4ee00";
+      default:
+        return "#98ee00";
+    }
+  }
+
+  //define the radius of the earthquake marker based on its magnitude.
+
+  function getRadius(magnitude) {
+    if (magnitude === 0) {
+      return 1;
+    }
+    return magnitude * 3;
+  }
+
+  // add GeoJSON layer to the map
+  L.geoJson(data, {
+    pointToLayer: function(feature, latlng) {
+      return L.circleMarker(latlng);
+    },
+    style: styleInfo,
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+    }
+
+  }).addTo(earthquakes);
+
+  earthquakes.addTo(map);
+
+  var legend = L.control({
+    position: "bottomright"
+  });
+
+
+  legend.onAdd = function() {
+    var div = L
+      .DomUtil
+      .create("div", "info legend");
+
+    var grades = [0, 1, 2, 3, 4, 5];
+    var colors = [
+      "#98ee00",
+      "#d4ee00",
+      "#eecc00",
+      "#ee9c00",
+      "#ea822c",
+      "#ea2c2c"
+    ];
+
+
+    for (var i = 0; i < grades.length; i++) {
+      div.innerHTML += "<i style='background: " + colors[i] + "'></i> " +
+        grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
+    }
+    return div;
+  }
+  
+  legend.addTo(map);
+
+  // retrive Tectonic Plate geoJSON data.
+  d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json",
+    function(platedata) {
+ 
+        L.geoJson(platedata, {
+        color: "orange",
+        weight: 2
+      })
+      .addTo(tectonicplates);
+
+      // add the tectonicplates layer to the map.
+      tectonicplates.addTo(map);
+    });
+});  
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+    
